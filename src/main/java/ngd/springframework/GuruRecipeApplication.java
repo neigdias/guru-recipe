35a63084d7e0e3baa519ecbaa710bbecf216eb0d
Claudinei Dias <neigdias@gmail.com>
2018-01-31 15:46:20 -0200

    Initial commit

---
+++ src/main/java/ngd/springframework/GuruRecipeApplication.java
@@ -0,0 +1,12 @@
+package ngd.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruRecipeApplication.class, args);
+	}
+}
---
+++ src/test/java/ngd/springframework/GuruRecipeApplicationTests.java
@@ -0,0 +1,16 @@
+package ngd.springframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuruRecipeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
