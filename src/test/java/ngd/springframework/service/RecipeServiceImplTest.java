c38554ec1ae86138a99060964dc4139da4ba7e11
Claudinei Dias <neigdias@gmail.com>
2018-02-16 16:46:42 -0200

    Added the test to validate findById method

--- src/test/java/ngd/springframework/service/RecipeServiceImplTest.java
+++ src/test/java/ngd/springframework/service/RecipeServiceImplTest.java
@@ -1,11 +1,15 @@
 package ngd.springframework.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.Before;
@@ -47,5 +51,20 @@ public class RecipeServiceImplTest {
 		
 		assertEquals(recipes.size(), 1);
 		verify(recipeRepository, times(1)).findAll();
+	}
+
+	@Test
+	public void findById() throws Exception {
+		Recipe recipe = new Recipe();
+		recipe.setId(1L);
+		Optional<Recipe> recipeOptional = Optional.of(recipe);
+		
+		when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+		
+		Recipe recipeReturned = recipeService.findById(1L);
+		
+		assertNotNull("Null recipe returned!", recipeReturned);
+		verify(recipeRepository, times(1)).findById(anyLong());
+		verify(recipeRepository, never()).findAll();
 	}
 }
